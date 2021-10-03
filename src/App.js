69938bc5c175387f598cd4ba059dbcdc26975bfe
Kaysul Anik <kaysul.i.anik@utu.fi>
2021-10-03 16:25:48 +0300

    Fetching date from API and setting routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import axios from 'axios';
+import Home from './Home';
+import UserDetails from './UserDetails';
 
 function App() {
-  return <div></div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        console.log('from APP', response.data);
+        setUsers(response.data);
+      })
+      .catch(error => console.log(error.message));
+  }, []);
+
+  return (
+    <Router>
+      <Route exact path="/">
+        <Home users={users} />
+      </Route>
+      <Route path="/users/:id">
+        <UserDetails users={users} />
+      </Route>
+    </Router>
+  );
 }
 
 export default App;
